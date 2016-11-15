9d81c043a65783580b29ab024b1dd328ed66e4dc
Eric Golde <eric@golde.org>
2016-11-15 09:02:01 -0800

    Create EditThatSite-TamperMonkey.js

---
+++ EditThatSite-TamperMonkey.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Edit That Site!
+// @namespace    http://eric.golde.org/
+// @version      1.0
+// @description  Enabled editing on most websites.
+// @author       Eric
+// @match        *://*/*
+// ==/UserScript==
+document.body.contentEditable ="true";
+document.designMode="on";
+void 0;
